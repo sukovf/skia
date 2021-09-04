fdd7e872a680a912dd26f57444c29af69db67923
sukovf <msoucek@seznam.cz>
2021-09-04 14:36:54 +0200

    Corner Path Effect fix

--- include/effects/SkDiscretePathEffect.h
+++ include/effects/SkDiscretePathEffect.h
@@ -8,11 +8,9 @@
 #ifndef SkDiscretePathEffect_DEFINED
 #define SkDiscretePathEffect_DEFINED
 
-#include "include/core/SkFlattenable.h"
 #include "include/core/SkPathEffect.h"
 
 /** \class SkDiscretePathEffect
-
     This path effect chops a path into discrete segments, and randomly displaces them.
 */
 class SK_API SkDiscretePathEffect : public SkPathEffect {
@@ -20,7 +18,6 @@ public:
     /** Break the path into segments of segLength length, and randomly move the endpoints
         away from the original path by a maximum of deviation.
         Note: works on filled or framed paths
-
         @param seedAssist This is a caller-supplied seedAssist that modifies
                           the seed value that is used to randomize the path
                           segments' endpoints. If not supplied it defaults to 0,
@@ -32,22 +29,7 @@ public:
     */
     static sk_sp<SkPathEffect> Make(SkScalar segLength, SkScalar dev, uint32_t seedAssist = 0);
 
-protected:
-    SkDiscretePathEffect(SkScalar segLength,
-                         SkScalar deviation,
-                         uint32_t seedAssist);
-    void flatten(SkWriteBuffer&) const override;
-    bool onFilterPath(SkPath* dst, const SkPath& src, SkStrokeRec*, const SkRect*) const override;
-
-private:
-    SK_FLATTENABLE_HOOKS(SkDiscretePathEffect)
-
-    SkScalar fSegLength, fPerterb;
-
-    /* Caller-supplied 32 bit seed assist */
-    uint32_t fSeedAssist;
-
-    using INHERITED = SkPathEffect;
+    static void RegisterFlattenables();
 };
 
 #endif
